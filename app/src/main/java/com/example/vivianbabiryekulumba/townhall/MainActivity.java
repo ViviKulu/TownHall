24e3d333f97c8ec891f2a3e665c1387c3ac125ed
Vivian Babirye <babimaji1111@gmail.com>
2018-08-01 18:28:53 -0400

    Built api for community boards Bx, Bk

--- app/src/main/java/com/example/vivianbabiryekulumba/townhall/MainActivity.java
+++ app/src/main/java/com/example/vivianbabiryekulumba/townhall/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.vivianbabiryekulumba.townhall;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.example.vivianbabiryekulumba.townhall.models.ZipCode;
+import com.example.vivianbabiryekulumba.townhall.network_service.NetworkService;
+import com.example.vivianbabiryekulumba.townhall.recycler.CommBoardAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity.class";
+    RecyclerView recyclerView;
+    Retrofit retrofit;
+    List<ZipCode> zipCodeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+
+        getRetrofit();
     }
+
+    public void getRetrofit() {
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://raw.githubusercontent.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        final NetworkService networkService = retrofit.create(NetworkService.class);
+
+        final Call<List<ZipCode>> commBoardCall = networkService.getCommBoardData();
+        commBoardCall.enqueue(new Callback<List<ZipCode>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<ZipCode>> call, @NonNull Response<List<ZipCode>> response) {
+                if (response.isSuccessful()) {
+                    zipCodeList = response.body();
+                    CommBoardAdapter adapter = new CommBoardAdapter(zipCodeList);
+                    adapter.notifyDataSetChanged();
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+                    recyclerView.setAdapter(adapter);
+                    recyclerView.setLayoutManager(linearLayoutManager);
+                    Log.d(TAG, "onResponse: success");
+                    Log.d(TAG, "onResponse: " + zipCodeList);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<ZipCode>> call, @NonNull Throwable t) {
+                t.printStackTrace();
+                Log.d(TAG, "onFailure: not successful");
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/vivianbabiryekulumba/townhall/models/ZipCode.java
@@ -0,0 +1,75 @@
+package com.example.vivianbabiryekulumba.townhall.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ZipCode {
+
+    @SerializedName("zip_code")
+    @Expose
+    private int zipCode;
+    @SerializedName("community_board")
+    @Expose
+    private String communityBoard;
+    @SerializedName("location")
+    @Expose
+    private String location;
+    @SerializedName("neighborhoods")
+    @Expose
+    private String neighborhoods;
+    @SerializedName("cb_info")
+    @Expose
+    private CbInfo cbInfo;
+    @SerializedName("neighborhood")
+    @Expose
+    private String neighborhood;
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCommunityBoard() {
+        return communityBoard;
+    }
+
+    public void setCommunityBoard(String communityBoard) {
+        this.communityBoard = communityBoard;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNeighborhoods() {
+        return neighborhoods;
+    }
+
+    public void setNeighborhoods(String neighborhoods) {
+        this.neighborhoods = neighborhoods;
+    }
+
+    public CbInfo getCbInfo() {
+        return cbInfo;
+    }
+
+    public void setCbInfo(CbInfo cbInfo) {
+        this.cbInfo = cbInfo;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+}
