94e8dc9189f71de6dc6861bc9efbe584e3123934
Vivian Babirye <babimaji1111@gmail.com>
2018-10-07 20:36:48 -0400

    register to vote online

--- app/src/main/java/com/example/vivianbabiryekulumba/townhall/main_fragments/RegisterToVoteOnlineFrag.java
+++ app/src/main/java/com/example/vivianbabiryekulumba/townhall/main_fragments/RegisterToVoteOnlineFrag.java
@@ -1,19 +1,30 @@
 package com.example.vivianbabiryekulumba.townhall.main_fragments;
 
 
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.vivianbabiryekulumba.townhall.R;
+
+import static android.support.constraint.Constraints.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterToVoteOnlineFrag extends Fragment {
 
+    private static final String TAG = "RegisterOnlineFrag";
+    WebView webView;
 
     public RegisterToVoteOnlineFrag() {
         // Required empty public constructor
@@ -24,7 +35,22 @@ public class RegisterToVoteOnlineFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_to_vote_online, container, false);
+        View view = inflater.inflate(R.layout.fragment_register_to_vote_online, container, false);
+
+        webView = view.findViewById(R.id.dmv_web_registration);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://voterreg.dmv.ny.gov/MotorVoter/");
+        webView.setWebViewClient(new TolerantWebViewClient());
+        return view;
     }
 
+
+    private class TolerantWebViewClient extends WebViewClient {
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+    }
 }
